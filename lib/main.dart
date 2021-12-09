8071532549f9fd8f264eeb35880b3bee0c5b6b41
MassiveMassimo <muhammad.hannan@ui.ac.id>
2021-12-09 22:00:47 +0700

    Changed font

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'Google Sans',
       ),
       routes: {
         // Main initial route
