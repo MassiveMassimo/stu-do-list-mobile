5c45ec1240ddce2923ea5a7ab485798f5d30638f
MassiveMassimo <muhammad.hannan@ui.ac.id>
2021-12-31 18:32:56 +0700

    Fixed fil mixup

--- lib/screens/study-community/screens/add_study_community.dart
+++ lib/screens/study-community/screens/add_study_community.dart
@@ -1,39 +1,186 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:mobile/screens/drawer_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/ask-a-mentor/models/comment.dart';
+import 'package:mobile/screens/study-community/providers/community_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:mobile/screens/study-community/models/community.dart';
 
-class CommentProvider with ChangeNotifier {
-  CommentProvider() {
-    this.fetchComments();
-  }
+class FormKomunitas extends StatefulWidget {
+  @override
+  _FormKomunitasState createState() => _FormKomunitasState();
+}
 
-  List<Comment> _comments = [];
+class _FormKomunitasState extends State<FormKomunitas> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  List<Comment> get comments {
-    return [..._comments];
-  }
+  String? matkul;
+  final kelas = TextEditingController();
+  String? prodi;
+  final info = TextEditingController();
+  final link = TextEditingController();
 
-  fetchComments() async {
-    final response = await http.get(Uri.parse(
-        'https://stu-do-list.herokuapp.com/ask-a-mentor/askamentor-jsoncomment'));
-    if (response.statusCode == 200) {
-      var data = json.decode(response.body) as List;
-      _comments = data.map<Comment>((json) => Comment.fromJson(json)).toList();
-      notifyListeners();
+  void onAdd() {
+    final String matkuladd = matkul!;
+    final String kelasadd = kelas.text;
+    final String prodiadd = prodi!;
+    final String infoadd = info.text;
+    final String linkadd = link.text;
+    if (matkuladd.isNotEmpty &&
+        kelasadd.isNotEmpty &&
+        prodiadd.isNotEmpty &&
+        infoadd.isNotEmpty &&
+        linkadd.isNotEmpty) {
+      final Community com = Community(
+        nama: matkuladd,
+        kelas: kelasadd,
+        prodi: prodiadd,
+        info: infoadd,
+        link: linkadd,
+      );
+
+      Provider.of<CommunityProvider>(context, listen: false).addCommunity(com);
+      Navigator.pop(context);
     }
   }
 
-  void addComment(Comment comment) async {
-    final response = await http.post(
-        Uri.parse(
-            'https://stu-do-list.herokuapp.com/ask-a-mentor/askamentor-jsoncomment'),
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode(comment));
-    if (response.statusCode == 201) {
-      comment.id = json.decode(response.body)["id"];
-      _comments.add(comment);
-      notifyListeners();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: DrawerScreen(),
+        appBar: AppBar(
+          title: Text('Komunitas Belajar',
+              style: TextStyle(color: Colors.grey.shade200)),
+          backgroundColor: Color(0xFF212529),
+        ),
+        body: Center(
+          child: Form(
+            key: formKey,
+            child: ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(
+                  left: 24,
+                  right: 24,
+                ),
+                scrollDirection: Axis.vertical,
+                children: [
+                  const SizedBox(height: 20),
+                  const Center(
+                    child: Text("Tambah Komunitas Belajar",
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 20),
+                  DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                        labelText: 'Pilih Mata Kuliah',
+                        border: OutlineInputBorder()),
+                    value: matkul,
+                    items: [
+                      'Aljabar Linear',
+                      'Metodologi Penelitian dan Penulisan Ilmiah',
+                      'Pemrograman Berbasis Platform',
+                      'Struktur Data & Algoritma',
+                      'Sistem Operasi untuk Sistem Informasi'
+                    ]
+                        .map((label) => DropdownMenuItem(
+                              child: Text(label),
+                              value: label,
+                            ))
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() => matkul = value!);
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Matkul tidak boleh kosong";
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                      controller: kelas,
+                      decoration: const InputDecoration(
+                        hintText: "Masukkan kelas",
+                        labelText: "Kelas ",
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.text,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Kelas tidak boleh kosong";
+                        }
+                      }),
+                  const SizedBox(height: 16),
+                  DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      labelText: 'Pilih Program Studi',
+                      border: OutlineInputBorder(),
+                    ),
+                    value: prodi,
+                    items: [
+                      'Ilmu Komputer',
+                      'Sistem Informasi',
+                      'Sistem Informasi & Ilmu Komputer'
+                    ]
+                        .map((label) => DropdownMenuItem(
+                              child: Text(label),
+                              value: label,
+                            ))
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() => prodi = value!);
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                      controller: info,
+                      decoration: const InputDecoration(
+                        hintText: "Masukkan keterangan tambahan",
+                        labelText: "Keterangan ",
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.text,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Keterangan tidak boleh kosong";
+                        }
+                      }),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                      controller: link,
+                      decoration: const InputDecoration(
+                        hintText: "Masukkan situs dalam bentul URL",
+                        labelText: "Situs Grup ",
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.text,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Situs tidak boleh kosong";
+                        }
+                      }),
+                  const SizedBox(height: 20),
+                  ButtonBar(
+                    buttonPadding: const EdgeInsets.only(right: 24, bottom: 24),
+                    alignment: MainAxisAlignment.center,
+                    children: [
+                      OutlinedButton(
+                          child: const Text("Batal"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: OutlinedButton.styleFrom(
+                              primary: Colors.red,
+                              side: const BorderSide(
+                                  color: Colors.red, width: 1))),
+                      ElevatedButton(
+                          onPressed: onAdd,
+                          child: const Text('Kirim'),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blueAccent.shade400))
+                    ],
+                  )
+                ]),
+          ),
+        ));
   }
 }
